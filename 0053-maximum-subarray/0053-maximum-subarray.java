d2afaff241469d653105bee23bbbee00d89c2af0
Mark Nagy Georgy <1900520@eng.asu.edu.eg>
2023-04-15 16:51:49 +0200

    Time: 1 ms (100.00%), Space: 52 MB (29.79%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -1,18 +1,17 @@
 class Solution {
-    public int maxSubArray(int[] nums) {
-        
-        int sum = 0 , maxi = Integer.MIN_VALUE;;
-        int i = 0 , j = 0;
-        while(j<nums.length){
-           
-            if(sum < 0 && nums[j] >= sum){
-                sum = 0;
-                i = j;
+    public static int maxSubArray(int[] nums) {
+
+            int sum = 0 , maxi = Integer.MIN_VALUE;;
+            int j = 0;
+            while(j<nums.length){
+
+                if(sum < 0 && nums[j] >= sum){
+                    sum = 0;
+                }
+                sum += nums[j];
+                maxi = Math.max(maxi , sum);
+                j++;
             }
-             sum += nums[j];
-            maxi = Math.max(maxi , sum);
-            j++;
+            return maxi;
         }
-        return maxi;
     }
-}
